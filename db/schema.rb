6d94f707c524a8b69021629cb152e1090ba481b6
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-07-31 15:14:11 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_060812) do
+ActiveRecord::Schema.define(version: 2019_07_31_061119) do
+
+  create_table "spans", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
