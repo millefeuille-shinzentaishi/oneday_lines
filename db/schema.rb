ac2827f9dacb6cebd219ebdfec44ef0c9ec84d0b
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-07-31 15:16:55 +0900

    create folder model controller migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_061119) do
+ActiveRecord::Schema.define(version: 2019_07_31_061600) do
+
+  create_table "folders", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "spans", force: :cascade do |t|
     t.date "start_date"
