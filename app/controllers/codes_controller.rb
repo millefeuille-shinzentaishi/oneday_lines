b7df3a24e71acb27b4a876834af2604a0c524ba1
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-07-31 20:20:39 +0900

    code count

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,4 +1,28 @@
 class CodesController < ApplicationController
+
   def new
+    @code = Code.new
+  end
+
+  def create
+    @code = Code.new(code_params)
+    @code.line = @code.content.lines.count
+    @code.folder_id = params[:folder_id]
+    if @code.save
+      redirect_to root_path
+    else
+      render new
+    end
+  end
+
+
+
+
+
+
+  private
+
+  def code_params
+    params.require(:code).permit(:name, :content)
   end
 end
--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,4 +1,29 @@
 class FoldersController < ApplicationController
   def new
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = Folder.new(folder_params)
+    if @folder.save
+      redirect_to @folder
+    else
+      render new
+    end
+  end
+
+  def show
+    @folder = Folder.find(params[:id])
+    @codes = Code.where(folder_id: @folder.id)
+  end
+
+
+
+
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:name)
   end
 end
